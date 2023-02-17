9c7e9206dd345d70419b3c7865524a5072d1fff0
Nodari <lucasbnodari@gmail.com>
2023-02-17 12:54:53 -0300

    feat: Swagger documentation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import connectDatabase from "./src/database/db.js";
 import userRouter from "./src/routes/user.route.js";
 import authRouter from "./src/routes/auth.route.js";
 import newsRouter from "./src/routes/news.route.js";
+import swaggerRouter from "./src/routes/swagger.router.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/news", newsRouter);
+app.use("/docs", swaggerRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
